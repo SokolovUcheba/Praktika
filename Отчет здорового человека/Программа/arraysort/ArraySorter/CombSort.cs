b8a4f98f44ffd5f50c838203a7007066b767a55e
SokolovUcheba <128531947+SokolovUcheba@users.noreply.github.com>
2023-03-24 08:39:20 +0300

    Add files via upload

---
+++ Отчет здорового человека/Программа/arraysort/ArraySorter/CombSort.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sokolov_CombSort
+{
+    public class CombSorter
+    {
+
+
+        public static void Swap(ref int value1, ref int value2)
+        {
+            var temp = value1;
+            value1 = value2;
+            value2 = temp;
+        }
+
+
+        public static int GetNextStep(int s)
+        {
+            s = s * 1000 / 1247;
+            return s > 1 ? s : 1;
+        }
+
+        public static int[] SortArray(int[] array)
+        {
+            var arrayLength = array.Length;
+            var currentStep = arrayLength - 1;
+
+            while (currentStep > 1)
+            {
+                for (int i = 0; i + currentStep < array.Length; i++)
+                {
+                    if (array[i] > array[i + currentStep])
+                    {
+                        Swap(ref array[i], ref array[i + currentStep]);
+                    }
+                }
+
+                currentStep = GetNextStep(currentStep);
+            }
+
+
+            for (var i = 1; i < arrayLength; i++)
+            {
+                var swapFlag = false;
+                for (var j = 0; j < arrayLength - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                        swapFlag = true;
+                    }
+                }
+
+                if (!swapFlag)
+                {
+                    break;
+                }
+            }
+
+            return array;
+        }
+
+
+        static int[] GetRandomArray(int length, int minValue, int maxValue)
+        {
+            var r = new Random();
+            var outputArray = new int[length];
+            for (var i = 0; i < outputArray.Length; i++)
+            {
+                outputArray[i] = r.Next(minValue, maxValue);
+            }
+
+            return outputArray;
+        }
+
+
+    }
+}
